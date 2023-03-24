b0ac41005165af5a9b9ba5db56ca13ceecedd23b
Fluttermagic <965030558@qq.com>
2023-03-24 16:29:32 +0800

    firstCommit

---
+++ Xie Zhengbin/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SearchBox } from './components/SearchingGames';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <br/><br/><br/><br/>
+      <SearchBox />
+    </div>
+  );
+}
+
+export default App;
